fb6c493ca3f8d6d3b8de8be75223d83519d61ac2
mmtapalla <gm.tapallamiguel@gmail.com>
2024-07-25 03:05:08 +0800

    Fix file references 2

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -36,7 +36,7 @@ controls.autoRotateSpeed = -10;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
-const loader = new GLTFLoader().setPath("../models/");
+const loader = new GLTFLoader().setPath("/../models/");
 loader.load(
     "scene.gltf",
     (gltf) => {
