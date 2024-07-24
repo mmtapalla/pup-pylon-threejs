6e4454952bbcf190250142f09ce22f201064bf71
mmtapalla <gm.tapallamiguel@gmail.com>
2024-07-25 02:38:09 +0800

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,80 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+);
+camera.position.set(10, 10, 10);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = true;
+controls.autoRotateSpeed = -10;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+const loader = new GLTFLoader().setPath("public/models/");
+loader.load(
+    "scene.gltf",
+    (gltf) => {
+        console.log("loading model");
+        const mesh = gltf.scene;
+
+        mesh.traverse((child) => {
+            if (child.isMesh) {
+                child.castShadow = true;
+                child.receiveShadow = true;
+            }
+        });
+
+        mesh.position.set(0, -2.5, 0);
+        scene.add(mesh);
+
+        // Add fade-out effect
+        const progressContainer = document.getElementById("progress-container");
+        progressContainer.classList.add("hidden");
+    },
+    (xhr) => {
+        console.log(`loading ${(xhr.loaded / xhr.total) * 100}%`);
+    },
+    (error) => {
+        console.error(error);
+    }
+);
+
+window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
